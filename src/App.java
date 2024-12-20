68a4c68afffb3333efe2b8d7d4aca8dd9809ccb4
Santiago Angel <santiagoangel@gmail.com>
2024-12-20 16:20:48 -0600

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,48 @@
+public class App {
+
+    // Str1 = azobecozebanc
+    // Str2 = abc
+
+    // Substring1: azobec
+    // Substring2: cozeba
+
+    // SubstringN: banc
+
+    // returns the smallest string that contains the characters in Str2
+
+    public static String findSubs(String one, String two) {
+
+        int len = one.length();
+        String output = "";
+
+        for (int i = 0; i < one.length(); i++) {
+            String sub = one.substring(i, one.length());
+
+            char chars[] = new char[two.length()];
+            two.getChars(0, two.length(), chars, 0);
+
+            boolean matchs = true;
+            for (char c : chars) {
+                if (!sub.contains(new Character(c).toString())) {
+                    matchs = false;
+                    break;
+                }
+
+            }
+
+            if (matchs) {
+
+                if (sub.length() < len) {
+                    output = sub;
+                    len = sub.length();
+                }
+            }
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(findSubs("azobecozebanc", "abc"));
+    }
+}
